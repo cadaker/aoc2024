ff945b8938865169d965a602e1bf96b668e9665f
Christian Adåker <cadaker@users.noreply.github.com>
2024-12-10 15:57:27 +0100

    Day 08 part 2

--- src/aoc2024/day08.clj
+++ src/aoc2024/day08.clj
@@ -14,24 +14,40 @@
      (= (first input) \.) (recur (rest input) row (inc col) width antennas)
      :else (recur (rest input) row (inc col) width (update antennas (first input) #(conj (or % []) [row col]))))))
 
-(defn pair-antinodes [[r1 c1] [r2 c2]]
-  (let [dr (- r1 r2), dc (- c1 c2)]
-    [[(+ r1 dr) (+ c1 dc)]
-     [(- r2 dr) (- c2 dc)]]))
-
-(defn group-antinodes [points]
-  (apply clojure.set/union
-         (for [m (range (count points))
-               n (range (inc m) (count points))]
-           (into #{} (pair-antinodes (nth points m) (nth points n))))))
-
 (defn is-inside? [height width [row col]]
   (and (>= row 0)
        (>= col 0)
        (< row height)
        (< col width)))
 
+(defn pair-antinodes [[r1 c1] [r2 c2]]
+  (let [dr (- r1 r2), dc (- c1 c2)]
+    [[(+ r1 dr) (+ c1 dc)]
+     [(- r2 dr) (- c2 dc)]]))
+
+(defn all-antinodes [height width [r1 c1] [r2 c2]]
+  (let [dr (- r1 r2), dc (- c1 c2)]
+    (concat (take-while (partial is-inside? height width)
+                        (iterate (fn [[r c]]
+                                   [(+ r dr) (+ c dc)])
+                                 [r1 c1]))
+            (take-while (partial is-inside? height width)
+                        (iterate (fn [[r c]]
+                                   [(- r dr) (- c dc)])
+                                 [r2 c2])))))
+
+(defn group-antinodes [antinode-gen points]
+  (apply clojure.set/union
+         (for [m (range (count points))
+               n (range (inc m) (count points))]
+           (into #{} (antinode-gen (nth points m) (nth points n))))))
+
+(defn count-and-filter [height width antinodes]
+  (count (filter (partial is-inside? height width) antinodes)))
+
 (defsolution day08 [input]
   (let [{width :width, height :height, antennas :antennas} (parse-input input)
-        antinode-groups (map group-antinodes (vals antennas))]
-    [(count (filter (partial is-inside? height width) (apply clojure.set/union antinode-groups)))]))
+        antinode-groups (map (partial group-antinodes pair-antinodes) (vals antennas))
+        all-antinode-groups (map (partial group-antinodes (partial all-antinodes height width)) (vals antennas))]
+    [(count-and-filter height width (apply clojure.set/union antinode-groups))
+     (count-and-filter height width (apply clojure.set/union all-antinode-groups))]))
