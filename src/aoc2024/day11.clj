274fc78f5facad8b2b4cf4286f489793985dc1b6
Christian Adåker <cadaker@users.noreply.github.com>
2024-12-13 18:27:19 +0100

    Day 11 part 2

--- src/aoc2024/day11.clj
+++ src/aoc2024/day11.clj
@@ -20,7 +20,27 @@
 (defn evolve-times [n stones]
   (nth (iterate evolve-all stones) n))
 
+(defn evolve-memoized [lookup stone n]
+  (let [answer (get lookup [stone n])]
+    (cond
+     (zero? n) [lookup 1]
+     (some? answer) [lookup answer]
+     :else (let [parts (evolve stone)
+                 [lookup* answer*] (reduce (fn [[lookup* answer*] part]
+                                             (let [[part-lookup part-answer] (evolve-memoized lookup* part (dec n))]
+                                               [part-lookup (+ answer* part-answer)]))
+                                           [lookup 0]
+                                           parts)]
+             [(assoc lookup* [stone n] answer*) answer*]))))
+
 (defsolution day11 [input]
   (let [stones (clojure.string/split (clojure.string/trimr input) #" ")]
   [(count (evolve-times 25 stones))
+   (loop [stones stones
+          lookup {}
+          total 0]
+     (if (seq stones)
+       (let [[lookup* n] (evolve-memoized lookup (first stones) 75)]
+         (recur (rest stones) lookup* (+ total n)))
+       total))
    ]))
